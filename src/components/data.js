69198bcd286a1457da7ddf01b819c108b4859fdc
Victor Godwin <conquerorsword36@yahoo.com>
2020-05-17 16:42:07 -0700

    fixes

--- src/components/data.js
+++ src/components/data.js
@@ -1,30 +1,33 @@
-[
+import im1 from '../assets/img/Teacher.svg'
+
+const data = [
     {
         "id" : 1,
         "name" : "How To Be A Better Teacher",
-        "image" : "./assets/img/Teacher.svg",
+        "image" : im1,
         "description" : "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum eligendi voluptatem veniam ut debitis ex quidem. Amet nisi sequi libero quasi nulla, ipsam nesciunt deserunt voluptas modi mollitia recusandae minima aspernatur inventore odio delectus ducimus obcaecati nam nihil officiis deleniti!",
-        "updated" : "Last Updated 58 seconds ago"  
+        "updated" : "Last Updated 58 seconds ago"
     } ,
     {
         "id" : 2,
         "name" : "Posture And Composure In Class",
-        "image" : "./assets/img/Teacher.svg",
+        "image" : im1,
         "description" : "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum eligendi voluptatem veniam ut debitis ex quidem. Amet nisi sequi libero quasi nulla, ipsam nesciunt deserunt voluptas modi mollitia recusandae minima aspernatur inventore odio delectus ducimus obcaecati nam nihil officiis deleniti!",
-        "updated" : "Last Updated 2 minutes ago"  
+        "updated" : "Last Updated 2 minutes ago"
     } ,
     {
         "id" : 3,
         "name" : "Relating With Students",
-        "image" : "./assets/img/Teacher.svg",
+        "image" : im1,
         "description" : "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum eligendi voluptatem veniam ut debitis ex quidem. Amet nisi sequi libero quasi nulla, ipsam nesciunt deserunt voluptas modi mollitia recusandae minima aspernatur inventore odio delectus ducimus obcaecati nam nihil officiis deleniti!",
-        "updated" : "Last Updated 7 minutes ago"  
+        "updated" : "Last Updated 7 minutes ago"
     } ,
     {
         "id" : 4,
         "name" : "Things Every Teacher Must Know",
-        "image" : "./assets/img/Teacher.svg",
+        "image" : im1,
         "description" : "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum eligendi voluptatem veniam ut debitis ex quidem. Amet nisi sequi libero quasi nulla, ipsam nesciunt deserunt voluptas modi mollitia recusandae minima aspernatur inventore odio delectus ducimus obcaecati nam nihil officiis deleniti!",
-        "updated" : "Last Updated 11 minutes ago"  
-    } 
+        "updated" : "Last Updated 11 minutes ago"
+    }
 ]
+export default data;
