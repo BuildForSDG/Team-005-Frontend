d8bc8141fc222142bb1db18e29cdf5eadb8416ea
jaybee4real <okorojijaybee1@gmail.com>
2020-05-23 20:08:11 +0100

    _beginning header section

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,35 @@
-import React, { Component } from "react";
-import homeHeaderImg from '../assets/img/download.jpeg'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
+import { Button } from 'react-bootstrap'
+
+
 
 class Home extends Component {
   state = {};
   render() {
     return (
-    <div>
+      <div>
         <div className="header">
-            <img className="headerImg" src={homeHeaderImg} alt="logo-img"/>
+          <div className="header-left-side">
+              <h2 className="header-title">Learn How To Become a Better Teacher With Olukọ</h2>
+              <Link to='/'>
+                  <Button>
+                        Level Up!
+                  </Button>
+              </Link>
+          </div>
+          <div className="header-right-side">
+              <div className="image-1"></div>
+              <div className="image-2"></div>
+              <div className="image-3"></div>
+          </div>
         </div>
-    </div>
-    )
+
+
+        {/* //////////////Main Content////////////////// */}
+        <div className="main-content"></div>
+      </div>
+    );
   }
 }
 
